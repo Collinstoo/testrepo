142f7c93b83ec83c757559d916f6d199d3e0bbbc
mwaicode <stevemurigu2@gmail.com>
2024-07-01 13:24:51 +0300

    main file change ondoa ruto

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ruto must goooooooo',
+      title: 'gen z operation ondoa ruto',
       theme: ThemeData(
         // This is the theme of your application.
         //
